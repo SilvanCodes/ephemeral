eb0901634fcc4b93e26ae569ac86290f0f45a35e
Silvan Buedenbender <silvancodes@gmail.com>
2022-03-04 11:06:12 +0100

    Use progress bar

--- lib/ephemeral_web/components/contact_list.ex
+++ lib/ephemeral_web/components/contact_list.ex
@@ -2,6 +2,7 @@ defmodule EphemeralWeb.Component.ContactList do
   use EphemeralWeb, :component
 
   import EphemeralWeb.Component.CopyToClipboard
+  import EphemeralWeb.Component.ProgressBar
 
   @spec contact_list(%{contacts: List.t()}) :: Phoenix.LiveView.Rendered.t()
   def contact_list(assigns) do
@@ -10,7 +11,7 @@ defmodule EphemeralWeb.Component.ContactList do
     <.box>
       <.stack>
         <p id={"id-#{key}"}><%= key %> <.copy_to_clipboard target={"#id-#{key}"} /></p>
-        <p style={"background: linear-gradient(270deg, white 0 #{lifeline}%, black #{lifeline}% 100%); height: var(--s-1); max-inline-size: 100%"}></p>
+        <.progress_bar progress={lifeline}/>
       </.stack>
     </.box>
     <% end %>
