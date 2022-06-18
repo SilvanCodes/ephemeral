55510c65edf5d8d9b16921ed121c73d4782e3182
Silvan Buedenbender <silvancodes@gmail.com>
2022-06-18 00:51:51 +0000

    Fix test

--- test/ephemeral_web/controllers/page_controller_test.exs
+++ test/ephemeral_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule EphemeralWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Ephemeral"
   end
 end
